fb011f24be138980f63e7ad3eb8d9a5285161463
Abdelhadikojok <119413261+Abdelhadikojok@users.noreply.github.com>
2023-03-20 22:33:00 +0200

    add excel file

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const xlsx = require("xlsx");
+const fs = require("fs");
 const app = express();
-var cors = require("cors");
+
 const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 
+const wb = xlsx.readFile("./data.xlsx", { dateNF: "mm/dd/yyyy" });
+
+const ws = wb.Sheets["products"];
+
+const data = xlsx.utils.sheet_to_json(ws, { raw: false });
+
+let newData = data.map((ele) => {
+  if (ele.paid === "TRUE") {
+    ele.paid = true;
+  }
+  if (ele.paid === "FALSE") {
+    ele.paid = false;
+  }
+  return ele;
+});
+
+fs.writeFileSync("./datajson.json", JSON.stringify(newData, null, 2));
+
 app.get("/", (req, res) => {
-  res.send("hi all");
+  res.send("hello");
 });
 
 app.listen(PORT, () => {
